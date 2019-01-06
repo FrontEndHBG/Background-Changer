9ca923e0069541949ac116db1d69ad3acb907e89
Max Carlquist <tenkaklet@gmail.com>
2019-01-06 22:23:06 +0100

    update to es6

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+const form = document.getElementById('form');
 
-var form = document.getElementById('form');
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    var color1 = document.getElementById('color1').value;
-    var color2 = document.getElementById('color2').value;
-    var color3 = document.getElementById('color3').value;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let color1 = document.querySelector('#color1').value;
+    let color2 = document.querySelector('#color2').value;
+    let color3 = document.querySelector('#color3').value;
 
     document.querySelector('.box1').style.backgroundColor = color1;
     document.querySelector('.box2').style.backgroundColor = color2;
     document.querySelector('.box3').style.backgroundColor = color3;
-    
 });
